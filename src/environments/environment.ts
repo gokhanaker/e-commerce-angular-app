317f4ae916ff880683fb58d787365cf3a98e0c91
Gokhan <gokhanaker@gmail.com>
2019-04-28 00:36:36 +0200

    for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyC95k6aDDtXCzuETAX1UwUNcIZ04UseKDI',
     authDomain: 'e-commerce-angular-app.firebaseapp.com',
