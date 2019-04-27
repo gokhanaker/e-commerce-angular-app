9582c720f9ad4a70a1a0b63689266fe7eba08aa2
Gokhan <gokhanaker@gmail.com>
2019-04-28 00:42:00 +0200

    for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC95k6aDDtXCzuETAX1UwUNcIZ04UseKDI',
+    authDomain: 'e-commerce-angular-app.firebaseapp.com',
+    databaseURL: 'https://e-commerce-angular-app.firebaseio.com',
+    projectId: 'e-commerce-angular-app',
+    storageBucket: 'e-commerce-angular-app.appspot.com',
+    messagingSenderId: '238234094644'
+  }
 };
